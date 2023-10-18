051ea781a2735ef8ed45502723df78e36f43a681
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2023-10-18 20:26:32 +0200

    Plassholder-overskrift

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Frid og Larus' brullup</h1>
+<p>Vi gifter oss 15. juni 2023!</p>
+<p>Mer info kommer fortløpende!</p>
