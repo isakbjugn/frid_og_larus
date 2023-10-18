185b098a5f5af9b96da230dccc7e6e8f14820a17
Isak Kyrre Lichtwarck Bjugn <isak.kyrre.lichtwarck.bjugn@sparebank1.no>
2023-10-18 22:18:05 +0200

    Landingsside med bilde

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,29 @@
-<h1>Frid og Larus' brullup</h1>
-<p>Vi gifter oss 15. juni 2023!</p>
-<p>Mer info kommer fortløpende!</p>
+<main>
+    <h1>Frid og Larus' bryllup</h1>
+    <p>Vi gifter oss 15. juni 2024!</p>
+    <picture>
+        <img src="frid-og-larus.jpg" alt="Smilende Frid og noe ukomfortabel Larus" />
+    </picture>
+    <p>Mer info kommer fortløpende!</p>
+</main>
+
+<style>
+    main {
+        max-width: 490px;
+        border-radius: 1rem;
+        margin: 0.5rem;
+        background-color: white;
+        padding: .5rem;
+        text-align: center;
+    }
+    @media screen and (min-width: 34rem) {
+        main {
+            margin: 1rem auto;
+        }
+    }
+    img {
+        width: 100%;
+    }
+</style>
+
+
